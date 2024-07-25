02653a9add270f278926d0a49215e10dc6ee1ca1
Aspirin2D <aspirin2ds@outlook.com>
2024-07-25 16:31:27 +0800

    wrangler

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-// @ts-check
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
-/** @type {import('next').NextConfig} */
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
+
 const nextConfig = {
   /* config options here */
 };
