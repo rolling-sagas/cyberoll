bf293d7d657d2de96c1d5744053f9237f7d03ee0
Aspirin2D <aspirin2ds@outlook.com>
2024-07-25 22:14:27 +0800

    add prisma

--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
 import Messages from "@/components/play/messages";
 import PinnedColumns from "@/components/play/pinned-columns";
 
+import { PrismaClient } from "@prisma/client";
+import { PrismaD1 } from "@prisma/adapter-d1";
+
 export const runtime = "edge";
 
 export default async function Page() {
+  const adapter = new PrismaD1(process.env.rsDb);
+  const prisma = new PrismaClient({ adapter });
+
+  const users = await prisma.user.findMany();
+  console.log(users);
+
   return (
     <>
       <Messages />
