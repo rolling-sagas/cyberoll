d3be3791a91ed8bcb603bee3a1ff315ad2c8538f
Aspirin2D <aspirin2ds@outlook.com>
2024-07-26 16:02:35 +0800

    add next-auth

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
 import Messages from "@/components/play/messages";
 import PinnedColumns from "@/components/play/pinned-columns";
 
-import prisma from "@/components/prisma/client";
+import prisma from "@/prisma/client";
 
 export const runtime = "edge";
 
 export default async function Page() {
-  const res = await prisma.session.findMany({
-    include: { system: true },
-  });
+  const res = await prisma.session.findMany();
   console.log(res);
 
   return (
