3fb6b585452a36050d1daac0c74f8ff08c7682cf
lizhiwei <lizhiwei1@dreame.tech>
2025-01-06 16:12:03 +0800

    remove layout dayjs

--- app/layout.js
+++ app/layout.js
@@ -14,30 +14,6 @@ import "./global.css";
 import { ToastPlaceholder } from "@/components/modal/toast-placeholder";
 import { AlertPlaceholder } from "@/components/modal/alert-placeholder";
 
-import relativeTime from 'dayjs/plugin/relativeTime';
-import updateLocale from 'dayjs/plugin/updateLocale';
-import dayjs from 'dayjs';
-dayjs.extend(relativeTime);
-dayjs.extend(updateLocale);
-
-dayjs.updateLocale('en', {
-  relativeTime: {
-    future: 'in %s',
-    past: '%s ago',
-    s: 'few secs',
-    m: '1m',
-    mm: '%dm',
-    h: '1h',
-    hh: '%dh',
-    d: '1d',
-    dd: '%dd',
-    M: '1m',
-    MM: '%dm',
-    y: '1y',
-    yy: '%dy',
-  },
-});
-
 export default function RootLayout({ children }) {
   const theme = useThemeStore((state) => state.theme);
 
