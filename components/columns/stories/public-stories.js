e1197fb45fa722ffc79bc0a6e87a2f46f51a096e
lizhiwei <lizhiwei1@dreame.tech>
2025-02-07 15:27:35 +0800

    feat: session list

--- components/columns/stories/public-stories.js
+++ components/columns/stories/public-stories.js
@@ -6,12 +6,16 @@ import Spinner from '../spinner';
 import PublicStoryItem from './story-item';
 
 import { getPublicStories } from '@/service/story';
+import ScrollSessions from '../sessions/scroll-sessions';
+
+import { getSessions } from '@/service/session';
 
 export default function Stories() {
   const [loading, setLoading] = useState(true);
   const [stories, setStories] = useState([]);
   const [page, setPage] = useState(0);
   const [total, setTotal] = useState(100);
+  const [sessions, setSessions] = useState([])
   const listStories = async (page) => {
     setLoading(true);
     try {
@@ -22,6 +26,15 @@ export default function Stories() {
       setLoading(false);
     }
   };
+
+  const listSessions = async () => {
+    const list = await getSessions()
+    setSessions(list)
+  }
+
+  useEffect(() => {
+    listSessions()
+  }, [])
 
   useEffect(() => {
     listStories(page);
@@ -36,25 +49,30 @@ export default function Stories() {
   }
 
   return (
-    <div className="overflow-y-auto overflow-x-hidden scrollbar-none w-full">
-      {stories.map((story) => (
-        <PublicStoryItem key={story.id} story={story} />
-      ))}
-      <div className="text-center">
-        {loading ? (
-          <div className="flex w-full h-20 items-center justify-center">
-            <Spinner />
-          </div>
-        ) : (
-          <Button
-            disabled={loading || page * 10 + 10 >= total}
-            onClick={() => setPage(page + 1)}
-            className="my-6"
-          >
-            {page * 10 + 10 >= total ? 'No more story' : 'Load more'}
-          </Button>
-        )}
+    <>
+      {
+        sessions && sessions.length ? <div className='mx-6 my-3'><ScrollSessions items={sessions} /></div> : null
+      }
+      <div className="overflow-y-auto overflow-x-hidden scrollbar-none w-full">
+        {stories.map((story) => (
+          <PublicStoryItem key={story.id} story={story} />
+        ))}
+        <div className="text-center">
+          {loading ? (
+            <div className="flex w-full h-20 items-center justify-center">
+              <Spinner />
+            </div>
+          ) : (
+            <Button
+              disabled={loading || page * 10 + 10 >= total}
+              onClick={() => setPage(page + 1)}
+              className="my-6"
+            >
+              {page * 10 + 10 >= total ? 'No more story' : 'Load more'}
+            </Button>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
