d88d492a8bc0f1c71f4dabc8a50c0c3f150808fc
Xuming <jinxm.alucard@gmail.com>
2025-04-21 16:31:11 +0800

    feat: update style

--- app/doc/page.js
+++ app/doc/page.js
@@ -4,7 +4,7 @@ import { MarkdownRenderer } from '@/components/markdown/markdown-renderer';
 export default function DocPage() {
   return (
     <div className="container mx-auto mt-[60px] h-[calc(100vh-60px)] overflow-y-auto overscroll-contain scrollbar-none flex flex-col items-center">
-      <div className="py-8 px-8 max-w-xl bg-rs-background-2">
+      <div className="py-8 px-8 max-w-xl">
         <MarkdownRenderer content={termsOfServicesMDContent} />
       </div>
     </div>
