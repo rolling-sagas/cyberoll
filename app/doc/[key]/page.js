5e7370cb3d424d8ad685c0599a3b707e52fe10a0
Xuming <jinxm.alucard@gmail.com>
2025-04-25 10:59:08 +0800

    feat: update doc url

--- app/doc/[key]/page.js
+++ app/doc/[key]/page.js
@@ -1,11 +1,24 @@
 import termsOfServicesMDContent from '@/components/doc/md/terms-of-services';
 import { MarkdownRenderer } from '@/components/markdown/markdown-renderer';
 
-export default function DocPage() {
+const defaultContent = 'Content not found';
+
+function getContentByKey(key) {
+  console.log({ key });
+  switch (key) {
+    case 'terms-of-services':
+      return termsOfServicesMDContent;
+    default:
+      return defaultContent;
+  }
+}
+
+export default function DocPage({ params }) {
+  const { key } = params;
   return (
     <div className="container mx-auto mt-[60px] h-[calc(100vh-60px)] overflow-y-auto overscroll-contain scrollbar-none flex flex-col items-center">
       <div className="py-8 px-8 max-w-xl">
-        <MarkdownRenderer content={termsOfServicesMDContent} />
+        <MarkdownRenderer content={getContentByKey(key)} />
       </div>
     </div>
   );
