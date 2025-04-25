9fc31b06a09499d787e66a4280b4c558d7f97f06
Xuming <jinxm.alucard@gmail.com>
2025-04-25 10:04:43 +0800

    feat: add prod firebase config

--- components/firbase/firebase-init.js
+++ components/firbase/firebase-init.js
@@ -4,6 +4,9 @@ import { initializeApp } from 'firebase/app';
 import { getPerformance } from 'firebase/performance';
 import { useEffect } from 'react';
 
+let app;
+let perf;
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -17,9 +20,13 @@ const firebaseConfig = {
 export default function FirebaseInit() {
   useEffect(() => {
     // 只在客户端初始化 Firebase
-    const app = initializeApp(firebaseConfig);
-    const perf = getPerformance(app);
-    console.log({ app, perf });
+    if (!app) {
+      app = initializeApp(firebaseConfig);
+      perf = getPerformance(app);
+      if (perf) {
+        console.log('Firebase Performance initialized');
+      }
+    }
 
     // 可以在这里添加其他 Firebase 初始化代码
   }, []);
