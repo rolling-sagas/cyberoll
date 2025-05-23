7addf2a64a7da94cd67adffeffe134b9b355fe1f
AlucPro <xmjin.micro@gmail.com>
2025-05-23 11:40:28 +0800

    feat: update rs_first_view

--- utils/utm-tracker.js
+++ utils/utm-tracker.js
@@ -6,11 +6,18 @@ import { getUtmParams, removeUtmParams, storeUtmParams } from '@/utils/utm';
 export async function utmTrack() {
   try {
     const utmParams = getUtmParams();
-    console.log('1. utmParams', utmParams);
+    const eventName = 'rs_first_view';
+    const hasViewed = localStorage.getItem(eventName);
+    if (hasViewed) {
+      removeUtmParams();
+      return;
+    }
     if (Object.keys(utmParams).length > 0) {
       storeUtmParams(utmParams);
       removeUtmParams();
-      await clientTrackEvent('rs_first_view', utmParams);
+
+      await clientTrackEvent(eventName, utmParams);
+      localStorage.setItem(eventName, String(new Date()));
     }
   } catch (error) {
     console.log('utm track failed: ', error);
